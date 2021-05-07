e3ddd317975c76bc840f06ebd56a2d31b331995f
Cat <ccallcottstevens@gmail.com>
2021-05-07 21:55:54 +1000

    created rules for pacman to move on grid and changed colours

--- app.js
+++ app.js
@@ -55,12 +55,48 @@ function createBoard() {
             squares[i].classList.add('pac-dot')
         } else if (layout[i] === 1) {
             squares[i].classList.add('wall')
+        } else if (layout[i] === 2) {
+            squares[i].classList.add('ghost-lair')
         } else if (layout[i] === 3) {
             squares[i].classList.add('power-pellet')
         }
     }
 }
 createBoard()
+
+//starting position of pac-man
+let pacmanCurrentIndex = 490
+
+squares[pacmanCurrentIndex].classList.add('pac-man')
+
+//move pac-man
+function movePacman(e) {
+
+    squares[pacmanCurrentIndex].classList.remove('pac-man')
+
+    switch(e.keyCode) {
+        case 37:
+            if (pacmanCurrentIndex % width !== 0 && !squares[pacmanCurrentIndex -1].classList.contains('wall') && !squares[pacmanCurrentIndex -1].classList.contains('ghost-lair') ) pacmanCurrentIndex -=1
+            break
+        case 38:
+            if(pacmanCurrentIndex - width >= 0 && !squares[pacmanCurrentIndex -width].classList.contains('wall') && !squares[pacmanCurrentIndex -width].classList.contains('ghost-lair')) pacmanCurrentIndex -= width
+            break
+        case 39:
+            if (pacmanCurrentIndex % width < width -1 && !squares[pacmanCurrentIndex +1].classList.contains('wall') && !squares[pacmanCurrentIndex +1].classList.contains('ghost-lair')) pacmanCurrentIndex +=1
+            break
+        case 40:
+            if(pacmanCurrentIndex + width < width * width && !squares[pacmanCurrentIndex +width].classList.contains('wall') && !squares[pacmanCurrentIndex +width].classList.contains('ghost-lair')) pacmanCurrentIndex +=width
+            break
+    }
+squares[pacmanCurrentIndex].classList.add('pac-man')
+
+//pacDotEaten()
+//powerPelletEaten()
+//checkForGameOver()
+//checkForWin()
+}
+document.addEventListener('keyup',movePacman)
+
 
 
 })
